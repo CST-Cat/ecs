bf3a492d39744a4ac9d8fea61157696ca40c53ba
Pascal Hou <cat_linus@outlook.com>
2026-08-21 00:35:11 +0800

    refactor(i18n): add compare-specific flag text

---
+++ internal/i18n/messages_compare_flags.go
@@ -0,0 +1,9 @@
+package i18n
+
+var compareFlagChinese = map[string]string{
+	"compare.flag.format": "对比结果输出格式：json、md、html",
+}
+
+var compareFlagEnglish = map[string]string{
+	"compare.flag.format": "comparison output formats: json, md, html",
+}
